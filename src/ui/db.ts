3473e79993ea0513bd8780fddbaf20126e117cc9
Vasilii Rogin <vasya@rogin.ru>
2018-07-23 19:13:06 +0300

    Revert "Some scary steps"
    This reverts commit bb8a6fbc3d248c885bd8ce0291faf9fdaa33664a.

--- src/ui/db.ts
+++ src/ui/db.ts
@@ -1,20 +1,13 @@
 import firebase from "firebase";
 
 import { Player } from "../lib/qmplayer/player";
-import { GameLog, GameState } from "../lib/qmplayer/funcs";
+import { GameLog } from "../lib/qmplayer/funcs";
 
 interface Config {
     player: Player;
     lastPlayedGame: string;
 }
 
-interface SavedGames {
-    [gameName: string]: GameState | undefined
-}
-
-interface WonGames {
-    [gameName: string]: GameLog | undefined
-}
 /*
 Here is firebase rules:
 {
@@ -38,13 +31,9 @@ Here is firebase rules:
 */
 const INDEXEDDB_NAME = "spaceranges";
 const INDEXEDDB_CONFIG_STORE_NAME = "config";
-const INDEXEDDB_SAVEDGAME_STORE_NAME = "savedgames";
-const INDEXESDV_WONGAMES_STORE_NAME = "wongames";
 
 const FIREBASE_USERS_PRIVATE = `usersPrivate`;
 const FIREBASE_USERS_PUBLIC = `usersPublic`;
-
-
 
 export async function getDb(app: firebase.app.App) {
     console.info("Starting to get db");
@@ -84,7 +73,6 @@ export async function getDb(app: firebase.app.App) {
         console.error(`Error with firebase: `, e);
     }
 
-    /*
     async function setPrivate(key: keyof Config, value: Config[typeof key]) {
         console.info(`setConfig key=${key} value=${value}`);
         const trx = db.transaction([INDEXEDDB_CONFIG_STORE_NAME], "readwrite");
@@ -107,10 +95,7 @@ export async function getDb(app: firebase.app.App) {
             console.error(`Error with firebase: `, e);
         }
     }
-    */
-    
 
-    /*
     async function getPrivate(key: keyof Config): Promise<Config[typeof key]> {
         const trx = db.transaction([INDEXEDDB_CONFIG_STORE_NAME], "readonly");
         const os = trx.objectStore(INDEXEDDB_CONFIG_STORE_NAME);
@@ -153,77 +138,6 @@ export async function getDb(app: firebase.app.App) {
         console.info(`getConfig key=${key} no firebase result`);
         return localResult;
     }
-    */
-
-    function createGetAndSet<T extends Object, U extends keyof T = keyof T>(
-        storeName: string) {
-            const set = async (key: U, value: T[U]) => {
-                console.info(`setConfig store=${storeName} key=${key} value=${value}`);
-                const trx = db.transaction([storeName], "readwrite");
-                const os = trx.objectStore(storeName);
-                const getReq = os.put(value, `${key}`);
-                await new Promise<void>((resolve, reject) => {
-                    getReq.onsuccess = e => resolve(getReq.result);
-                    getReq.onerror = e => reject(new Error(getReq.error.toString()));
-                });
-        
-                try {
-                    if (firebaseUser) {
-                        console.info(`setConfig store=${storeName} key=${key} value=${value} saving to firebase`);
-                        await app
-                            .database()
-                            .ref(`${FIREBASE_USERS_PRIVATE}/${firebaseUser.uid}/${key}`)
-                            .set(value);
-                    }
-                } catch (e) {
-                    console.error(`Error with firebase: `, e);
-                }
-            };
-
-        const get = async (key: U): Promise<T[typeof key]> =>  {
-            const trx = db.transaction([storeName], "readonly");
-            const os = trx.objectStore(storeName);
-            const getReq = os.get(key);
-            const localResult = await new Promise<T[typeof key]>(
-                (resolve, reject) => {
-                    getReq.onsuccess = e => resolve(getReq.result);
-                    getReq.onerror = e =>
-                        reject(new Error(getReq.error.toString()));
-                }
-            );
-            console.info(`getConfig store=${storeName} key=${key} localResult=${localResult}`);
-    
-            try {
-                const firebaseResult = await new Promise<
-                    T[U]| undefined
-                >(
-                    resolve =>
-                        firebaseUser
-                            ? app
-                                  .database()
-                                  .ref(
-                                      `${FIREBASE_USERS_PRIVATE}/${
-                                          firebaseUser.uid
-                                      }/${key}`
-                                  )
-                                  .once("value", snapshot =>
-                                      resolve(snapshot ? snapshot.val() : undefined)
-                                  )
-                            : resolve(undefined)
-                );
-                if (firebaseResult !== undefined) {
-                    console.info(`getConfig key=${key} firebaseResult=${localResult}`);
-                    await set(key, firebaseResult);
-                    return firebaseResult;
-                }
-            } catch (e) {
-                console.error(`Error with firebase: `, e);
-            }
-            console.info(`getConfig key=${key} no firebase result`);
-            return localResult;
-        }
-        return {get, set}
-    }
 
     function setWonGame(gameName: string, proof: GameLog) {
         // todo
@@ -248,10 +162,8 @@ export async function getDb(app: firebase.app.App) {
 
     }
 
-    const privatevalues = createGetAndSet<Config>(INDEXEDDB_CONFIG_STORE_NAME);
-
     return {
-        getPrivate: privatevalues.get,
-        setPrivate: privatevalues.set,
+        getPrivate,
+        setPrivate
     };
 }
