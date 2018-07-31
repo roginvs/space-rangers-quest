92555f7ae1fca5f6191dfe502e283346a0a5aabc
Vasilii Rogin <vasya@rogin.ru>
2018-08-01 00:54:54 +0300

    Added new version info popup

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -29,3 +29,29 @@ getJson(INDEX_JSON)
             root
         )
     );
+
+
+$('body').append(`
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+ aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Новая версия!</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      Друзья! Вышла новая версия плеера <a href="https://spacerangers.gitlab.io">https://spacerangers.gitlab.io</a>
+      <br/>Прошу тестить. Старая будет здесь и никуда не денется.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Хорошо</button>        
+        <button type="button" class="btn btn-primary" onClick='location.href="https://spacerangers.gitlab.io"'>Перейти на новую</button>
+      </div>
+    </div>
+  </div>
+</div>
+`);
+$('#exampleModal').modal();
