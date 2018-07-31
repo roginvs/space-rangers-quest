efe3226570f565e255bd797378788a06562e0d3e
Vasilii Rogin <vasya@rogin.ru>
2018-08-01 00:43:25 +0300

    Changed to merging version1 to gitlab

--- merge-and-upload-to-gitlab.sh
+++ merge-and-upload-to-gitlab.sh
@@ -2,9 +2,9 @@
 set -e
 echo "Checking out saturated branch"
 git checkout saturated
-echo "Merging master"
-git merge --no-edit master
+echo "Merging version1"
+git merge --no-edit version1
 echo "Uploading"
 git push -u gitlab saturated:master
-echo "Checkouting master branch"
-git checkout master
+echo "Checkouting version1 branch"
+git checkout version1
