e2e2949e9ade26d6f583b2badd22831e4b6d2a48
Vasilii Rogin <vasya@rogin.ru>
2018-07-31 21:53:57 +0300

    Updated for merging to gitlab

--- merge-and-upload-to-gitlab.sh
+++ merge-and-upload-to-gitlab.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-echo "Checking out saturated branch"
-git checkout saturated
+echo "Checking out saturated2 branch"
+git checkout saturated2
 echo "Merging master"
 git merge --no-edit master
 echo "Uploading"
-git push -u gitlab saturated:master
+git push -u gitlab2 saturated2:master
 echo "Checkouting master branch"
 git checkout master
