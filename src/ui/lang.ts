5738f8ab59c59a76e583e5eb11cf726e64eb5752
Vasilii Rogin <vasya@rogin.ru>
2018-08-28 17:42:45 +0300

    Update language guess

--- src/ui/lang.ts
+++ src/ui/lang.ts
@@ -239,7 +239,7 @@ export function getLang(lang: Lang) {
 
 export function guessBrowserLang(): Lang {
     const browserLanguages = navigator.languages || [
-        navigator.language || (navigator as any).userLanguage || "sv"
+        navigator.language || (navigator as any).userLanguage || "ru"
     ];
     console.info(
         `languages=${
@@ -255,7 +255,7 @@ export function guessBrowserLang(): Lang {
             return "rus";
         }
     }
-    if (navigator.userAgent.indexOf('Googlebot') > -1) { // Чтобы google русский вариант индексировал
+    if ((window as any).callPhantom || (window as any)._phantom) { // Чтобы google русский вариант индексировал
         return "rus"
     }
     return "eng";
