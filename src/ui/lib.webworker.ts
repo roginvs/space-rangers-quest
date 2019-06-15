e4e4a002a267496b5ece2fe26ec0cf6fea43f03f
Vasilii Rogin <vasilii@giosg.com>
2019-06-15 22:32:34 +0300

    Fixing tslint

--- src/ui/lib.webworker.ts
+++ src/ui/lib.webworker.ts
@@ -1,3 +1,5 @@
+// tslint:disable
+
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation. All rights reserved. 
 Licensed under the Apache License, Version 2.0 (the "License"); you may not use
