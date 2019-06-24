a92a1d909ef3830b03c70e5e346b195e825a6413
Vasilii Rogin <vasilii@giosg.com>
2019-06-24 22:51:38 +0300

    Enable treeshaking

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 const devServer /*: webpackDevServer.Configuration */ = {
   contentBase: "./built-web",
-  port: 4099
+  port: 4099,
   // inline: false, // for serviceWorker development tests
 };
 
@@ -22,8 +22,8 @@ const config = (env, argv) => {
 
         new ForkTsCheckerWebpackPlugin({
           tslint: true,
-          memoryLimit: 4096 // 2048 is default and this is not enough?
-        })
+          memoryLimit: 4096, // 2048 is default and this is not enough?
+        }),
       ]
     : [];
 
@@ -31,39 +31,39 @@ const config = (env, argv) => {
     entry: {
       index: "./src/ui/index.tsx",
       worker: "./src/ui/worker/worker.ts",
-      serviceWorker: "./src/ui/serviceWorker.ts"
+      serviceWorker: "./src/ui/serviceWorker.ts",
     },
 
     output: {
       filename: "[name].js",
       chunkFilename: "[id].js",
-      path: __dirname + "/built-web"
+      path: __dirname + "/built-web",
     },
 
     plugins: [
       ...developmentModePlugins,
       new CopyWebpackPlugin([
         {
-          from: "src/webstatic"
-        }
+          from: "src/webstatic",
+        },
       ]),
       new MiniCssExtractPlugin({
         // Options similar to the same options in webpackOptions.output
         // both options are optional
         filename: "[name].css",
-        chunkFilename: "[id].css"
+        chunkFilename: "[id].css",
       }),
       //new WebpackVersionHashPlugin({
       //    filename: 'version.json',
       //    include_date: true
       //}),
       new webpack.DefinePlugin({
-        __VERSION__: JSON.stringify(new Date().toISOString())
+        __VERSION__: JSON.stringify(new Date().toISOString()),
       }),
       new ServiceWorkerWebpackPlugin({
         entry: "./src/ui/serviceWorker.ts",
-        filename: "sw.js"
-      })
+        filename: "sw.js",
+      }),
     ],
 
     module: {
@@ -72,7 +72,7 @@ const config = (env, argv) => {
           enforce: "pre",
           test: /\.(js|ts|tsx)$/,
           loader: "source-map-loader",
-          exclude: /node_modules/
+          exclude: /node_modules/,
         },
         {
           test: /\.tsx?$/,
@@ -82,33 +82,35 @@ const config = (env, argv) => {
             compilerOptions: MODE_DEVELOPMENT
               ? {
                   target: "ES2018",
-                  downlevelIteration: false
+                  downlevelIteration: false,
+                  module: "ES6",
+                  moduleResolution: "node",
                 }
-              : {}
+              : { module: "ES6", moduleResolution: "node" },
           },
-          exclude: /node_modules/
+          exclude: /node_modules/,
         },
 
         {
           test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader"]
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
         },
 
         {
           test: /\.(png|svg|jpg|gif)$/,
-          use: ["file-loader?name=img/[hash].[ext]"]
+          use: ["file-loader?name=img/[hash].[ext]"],
         },
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: ["file-loader?name=fonts/[hash].[ext]"]
-        }
-      ]
+          use: ["file-loader?name=fonts/[hash].[ext]"],
+        },
+      ],
     },
     resolve: {
-      extensions: [".ts", ".tsx", ".js", ".json"]
+      extensions: [".ts", ".tsx", ".js", ".json"],
     },
 
-    devServer
+    devServer,
   };
 };
 
