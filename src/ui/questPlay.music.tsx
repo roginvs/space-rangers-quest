0707b4409c5c84e831be2747aaa4eeaa5d293d9e
Vasilii Rogin <vasya@rogin.ru>
2021-05-14 11:12:42 +0300

    Add musicplay component

---
+++ src/ui/questPlay.music.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+
+function getRandom<T>(list: T[]): T {
+  const i = Math.floor(Math.random() * list.length);
+  return list[i]
+}
+
+export function Music({ urls }: { urls: string[] }) {
+  const audioElement = React.useRef<HTMLAudioElement | null>(null);
+  const [url, setUrl] = React.useState(getRandom(urls));
+
+  React.useEffect(() => {
+    const play = () => {
+      if (!audioElement.current) {
+        return
+      }
+      void audioElement.current.play();
+    }
+    document.addEventListener("click", play);
+    document.addEventListener("touchstart", play);
+    play();
+    return () => {
+      document.removeEventListener("click", play);
+      document.removeEventListener("touchstart", play);
+    }
+  }, [url]);
+
+  return <audio
+    autoPlay={true}
+    controls={false}
+    onEnded={() => {
+      setUrl(getRandom(urls))
+    }}
+    src={url}
+    ref={
+      audioElement
+    }
+  />
+}
