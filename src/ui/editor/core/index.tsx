859fcc1e1744907ccd5a9d0828723e5a78fe3952
Vasilii Rogin <vasya@rogin.ru>
2021-05-23 14:44:18 +0300

    Experiment with editor

--- src/ui/editor/core/index.tsx
+++ src/ui/editor/core/index.tsx
@@ -1,4 +1,6 @@
+import classNames from "classnames";
 import * as React from "react";
+import { assertNever } from "../../../lib/formula/calculator";
 import { Quest } from "../../../lib/qmplayer/funcs";
 
 export interface EditorCoreProps {
@@ -7,6 +9,55 @@ export interface EditorCoreProps {
   //onExit: () => void,
 }
 
+export const EDITOR_MODES = ["select", "move", "newLocation", "newJump", "remove"] as const;
+export type EditorMode = typeof EDITOR_MODES[number];
+
 export function EditorCore({ quest, onChange }: EditorCoreProps) {
-  return <div>todo</div>;
+  const [mode, setMode] = React.useState<EditorMode>("select");
+
+  return (
+    <div style={{ width: "100%", height: "100vh", display: "flex", flexDirection: "column" }}>
+      <div>
+        {EDITOR_MODES.map(candidateMode => (
+          <button
+            key={candidateMode}
+            className={classNames("btn", mode === candidateMode ? "btn-info" : "btn-light")}
+            onClick={() => setMode(candidateMode)}
+          >
+            {candidateMode === "move" ? (
+              <i className="fa fa-arrows" title="Двигать (2)" />
+            ) : candidateMode === "select" ? (
+              <i className="fa fa-mouse-pointer" title="Выделять (1)" />
+            ) : candidateMode === "newJump" ? (
+              <i className="fa fa-arrows-h" title="Новый переход (4)" />
+            ) : candidateMode === "newLocation" ? (
+              <i className="fa fa-circle" title="Новая локация (3)" />
+            ) : candidateMode === "remove" ? (
+              <i className="fa fa-remove" title="Удалить (5)" />
+            ) : (
+              assertNever(candidateMode)
+            )}
+          </button>
+        ))}
+        <span className="mx-2" />
+        {/*
+    <button className="btn btn-light" disabled={!store.canUndo} onClick={() => store.undo()}>
+      <i className="fa fa-undo" />
+    </button>
+    <button className="btn btn-light" disabled={!store.canRedo} onClick={() => store.redo()}>
+      <i className="fa fa-undo fa-flip-horizontal" />
+    </button>
+    */}
+      </div>
+      <div
+        style={{
+          flexGrow: 1,
+          alignSelf: "stretch",
+          overflow: "scroll",
+        }}
+      >
+        <asdas>
+      </div>
+    </div>
+  );
 }
