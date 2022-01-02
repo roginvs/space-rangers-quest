b3a2bc2787e3c8c89aabf43b04ff6853da542b03
Vasilii Rogin <vasya@rogin.ru>
2022-01-02 23:44:45 +0200

    Adding location hover

--- src/ui/editor/core/hovers/location.tsx
+++ src/ui/editor/core/hovers/location.tsx
@@ -3,5 +3,50 @@ import { DeepImmutable } from "../../../../lib/qmplayer/deepImmutable";
 import { Location } from "../../../../lib/qmreader";
 
 export function LocationHover({ location }: { location: DeepImmutable<Location> }) {
-  return <div>Location text: {location.texts[0]}</div>;
+  const shortInfo = location.isStarting
+    ? "Стартовая локация"
+    : location.isSuccess
+    ? "Победная локация"
+    : location.isFailyDeadly
+    ? "Смертельная локация"
+    : location.isFaily
+    ? "Провальная локация"
+    : location.isEmpty
+    ? "Пустая локация"
+    : location.length === 0
+    ? "Пустая локация (нет теста)"
+    : "Промежуточная локация";
+
+  const divider = <div>----------------------------------------------</div>;
+
+  const firstNotEmptyText = location.texts.find((text) => text);
+
+  const MAX_TEXT_CHARS = 300;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ display: "flex" }}>
+        <span>{location.texts.length > 1 ? "M" : ""}</span>
+        <span style={{ marginLeft: "auto" }}>L {location.id}</span>
+      </div>
+      {divider}
+      <div>- {shortInfo} -</div>
+      {divider}
+      <div
+        style={{
+          textAlign: "center",
+          color: "#0000EE",
+        }}
+      >
+        {firstNotEmptyText ? firstNotEmptyText.slice(0, MAX_TEXT_CHARS) : ""}{" "}
+        {firstNotEmptyText && firstNotEmptyText.length > MAX_TEXT_CHARS ? "..." : ""}
+      </div>
+    </div>
+  );
 }
