b7801616dd0924882aeebb861ae3a170ab4c93d1
Vasilii Rogin <vasya@rogin.ru>
2022-01-03 21:23:18 +0200

    Update colors

--- src/ui/editor/colors.ts
+++ src/ui/editor/colors.ts
@@ -1,14 +1,42 @@
 export const colors = {
   background: "#aaaaaa",
   location: {
-    starting: "#5455fd",
+    starting: "#5555ff",
     final: "#00ff00",
     intermediate: "#ffffff",
     empty: "#004101",
-    fail: "#d10201",
+    fail: "#d20000",
   },
   jump: {
-    line: "black",
-    arrow: "black",
+    //line: "black",
+    // arrow: "black",
+    withDescription: {
+      begin: "#ffffff",
+      end: "#0000ff",
+    },
+    withoutDescription: {
+      begin: "#00ddff",
+      end: "#00aaaa",
+    },
+    sameTextWithDescription: {
+      begin: "#ffffff",
+      end: "#800000",
+    },
+    sameTextWithoutDescription: {
+      begin: "#f5c896",
+      end: "#800000",
+    },
+    empty: {
+      begin: "#ffffff",
+      end: "#004000",
+    },
+    /* what's this? sometimes jump arrow is painted so */
+    arrow: "#0000ff",
   },
+
+  backgroundJumpsStats: "#e7e298",
+  unlimitedPassingJump: "#0000ff",
+  limitedPassingJump: "#ff0000",
+  selectionOutbound: "#000000",
+  selectionInbound: "#ff8040",
 } as const;
