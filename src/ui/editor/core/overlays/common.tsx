87df0696de0543dc3a946aa9e991f530eb53c593
Vasilii Rogin <vasya@rogin.ru>
2022-01-04 17:59:39 +0200

    Location edit steps

--- src/ui/editor/core/overlays/common.tsx
+++ src/ui/editor/core/overlays/common.tsx
@@ -1,7 +1,7 @@
 import classNames from "classnames";
 import * as React from "react";
 import { DeepImmutable } from "../../../../lib/qmplayer/deepImmutable";
-import { Media, ParameterChange, QMParam } from "../../../../lib/qmreader";
+import { Media, ParameterChange, ParameterShowingType, QMParam } from "../../../../lib/qmreader";
 import { checkFormula } from "../checkFormula";
 
 export function MediaEdit({ media, setMedia }: { media: Media; setMedia: (media: Media) => void }) {
@@ -53,142 +53,183 @@ interface ParamChangeProps {
 }
 export function ParamChangeTypeEdit({ change, onChange, param }: ParamChangeProps) {
   return (
-    <div className="row">
-      <div className="col-6">
-        <div className="row form-group">
-          <div className="col-6 form-check xform-check-inline">
-            <label className="form-check-label">
-              <input
-                className="form-check-input"
-                type="radio"
-                value="option1"
-                checked={
-                  !change.isChangePercentage && !change.isChangeValue && !change.isChangeFormula
-                }
-                onClick={() =>
-                  onChange({
-                    ...change,
-                    isChangePercentage: false,
-                    isChangeValue: false,
-                    isChangeFormula: false,
-                  })
-                }
-              />
-              Единицы
-            </label>
+    <>
+      <div className="row">
+        <div className="col-6">
+          <div className="row form-group">
+            <div className="col-6 form-check xform-check-inline">
+              <label className="form-check-label">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  value="option1"
+                  checked={
+                    !change.isChangePercentage && !change.isChangeValue && !change.isChangeFormula
+                  }
+                  onClick={() =>
+                    onChange({
+                      ...change,
+                      isChangePercentage: false,
+                      isChangeValue: false,
+                      isChangeFormula: false,
+                    })
+                  }
+                />
+                Единицы
+              </label>
+            </div>
+            <div className="col-6 form-check xform-check-inline">
+              <label className="form-check-label">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  value="option2/"
+                  checked={change.isChangePercentage}
+                  onClick={() =>
+                    onChange({
+                      ...change,
+                      isChangePercentage: true,
+                      isChangeValue: false,
+                      isChangeFormula: false,
+                    })
+                  }
+                />
+                Проценты
+              </label>
+            </div>
           </div>
-          <div className="col-6 form-check xform-check-inline">
-            <label className="form-check-label">
-              <input
-                className="form-check-input"
-                type="radio"
-                value="option2/"
-                checked={change.isChangePercentage}
-                onClick={() =>
-                  onChange({
-                    ...change,
-                    isChangePercentage: true,
-                    isChangeValue: false,
-                    isChangeFormula: false,
-                  })
-                }
-              />
-              Проценты
-            </label>
+          <div className="row form-group">
+            <div className="col-6 form-check xform-check-inline">
+              <label className="form-check-label">
+                {" "}
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  value="option3"
+                  checked={change.isChangeValue}
+                  onClick={() =>
+                    onChange({
+                      ...change,
+                      isChangePercentage: false,
+                      isChangeValue: true,
+                      isChangeFormula: false,
+                    })
+                  }
+                />
+                Значение
+              </label>
+            </div>
+            <div className="col-6 form-check xform-check-inline">
+              <label className="form-check-label">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  value="option4"
+                  checked={change.isChangeFormula}
+                  onClick={() =>
+                    onChange({
+                      ...change,
+                      isChangePercentage: false,
+                      isChangeValue: false,
+                      isChangeFormula: true,
+                    })
+                  }
+                />
+                Выражение
+              </label>
+            </div>
           </div>
         </div>
-        <div className="row form-group">
-          <div className="col-6 form-check xform-check-inline">
-            <label className="form-check-label">
-              {" "}
-              <input
-                className="form-check-input"
-                type="radio"
-                value="option3"
-                checked={change.isChangeValue}
-                onClick={() =>
-                  onChange({
-                    ...change,
-                    isChangePercentage: false,
-                    isChangeValue: true,
-                    isChangeFormula: false,
-                  })
-                }
-              />
-              Значение
-            </label>
-          </div>
-          <div className="col-6 form-check xform-check-inline">
-            <label className="form-check-label">
-              <input
-                className="form-check-input"
-                type="radio"
-                value="option4"
-                checked={change.isChangeFormula}
-                onClick={() =>
-                  onChange({
-                    ...change,
-                    isChangePercentage: false,
-                    isChangeValue: false,
-                    isChangeFormula: true,
-                  })
-                }
-              />
-              Выражение
-            </label>
-          </div>
-        </div>
-      </div>
-      <div className="col-6">
-        {!change.isChangeFormula && !change.isChangeValue && !change.isChangePercentage ? (
-          <input
-            type="range"
-            className="form-range w-100"
-            min={param.min - param.max}
-            max={param.max - param.min}
-            value={change.change}
-            onChange={(e) => onChange({ ...change, change: parseInt(e.target.value) })}
-          />
-        ) : change.isChangeValue ? (
-          <input
-            type="range"
-            className="form-range w-100"
-            min={param.min}
-            max={param.max}
-            value={change.change}
-            onChange={(e) => onChange({ ...change, change: parseInt(e.target.value) })}
-          />
-        ) : change.isChangePercentage ? (
-          <input
-            type="range"
-            className="form-range w-100"
-            min={-100}
-            max={+100}
-            value={change.change}
-            onChange={(e) => onChange({ ...change, change: parseInt(e.target.value) })}
-          />
-        ) : change.isChangeFormula ? (
-          <input type="range" className="form-range w-100" max={2} min={0} value={1} disabled />
-        ) : null}
+        <div className="col-6">
+          {!change.isChangeFormula && !change.isChangeValue && !change.isChangePercentage ? (
+            <input
+              type="range"
+              className="form-range w-100"
+              min={param.min - param.max}
+              max={param.max - param.min}
+              value={change.change}
+              onChange={(e) => onChange({ ...change, change: parseInt(e.target.value) })}
+            />
+          ) : change.isChangeValue ? (
+            <input
+              type="range"
+              className="form-range w-100"
+              min={param.min}
+              max={param.max}
+              value={change.change}
+              onChange={(e) => onChange({ ...change, change: parseInt(e.target.value) })}
+            />
+          ) : change.isChangePercentage ? (
+            <input
+              type="range"
+              className="form-range w-100"
+              min={-100}
+              max={+100}
+              value={change.change}
+              onChange={(e) => onChange({ ...change, change: parseInt(e.target.value) })}
+            />
+          ) : change.isChangeFormula ? (
+            <input type="range" className="form-range w-100" max={2} min={0} value={1} disabled />
+          ) : null}
 
-        {!change.isChangeFormula ? (
-          <input
-            type="number"
-            className={classNames("form-control w-100")}
-            value={change.change}
-            onChange={(e) => onChange({ ...change, change: parseInt(e.target.value) })}
-          />
-        ) : (
-          <input
-            className={classNames(
-              "form-control w-100",
-              checkFormula(change.changingFormula) ? "is-invalid" : "",
-            )}
-            value={change.changingFormula}
-            onChange={(e) => onChange({ ...change, changingFormula: e.target.value })}
-          />
-        )}
+          {!change.isChangeFormula ? (
+            <input
+              type="number"
+              className={classNames("form-control w-100")}
+              value={change.change}
+              onChange={(e) => onChange({ ...change, change: parseInt(e.target.value) })}
+            />
+          ) : (
+            <input
+              className={classNames(
+                "form-control w-100",
+                checkFormula(change.changingFormula) ? "is-invalid" : "",
+              )}
+              value={change.changingFormula}
+              onChange={(e) => onChange({ ...change, changingFormula: e.target.value })}
+            />
+          )}
+        </div>
       </div>
-    </div>
+
+      <div className="d-flex justify-content-center">
+        <div className="form-check form-check-inline">
+          <label className="form-check-label">
+            <input
+              className="form-check-input"
+              type="radio"
+              value="option1"
+              checked={change.showingType === ParameterShowingType.НеТрогать}
+              onClick={() => onChange({ ...change, showingType: ParameterShowingType.НеТрогать })}
+            />
+            Не трогать
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <label className="form-check-label">
+            <input
+              className="form-check-input"
+              type="radio"
+              value="option1"
+              checked={change.showingType === ParameterShowingType.Скрыть}
+              onClick={() => onChange({ ...change, showingType: ParameterShowingType.Скрыть })}
+            />
+            Скрыть
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <label className="form-check-label">
+            <input
+              className="form-check-input"
+              type="radio"
+              value="option1"
+              checked={change.showingType === ParameterShowingType.Показать}
+              onClick={() => onChange({ ...change, showingType: ParameterShowingType.Показать })}
+            />
+            Показать
+          </label>
+        </div>
+      </div>
+    </>
   );
 }
