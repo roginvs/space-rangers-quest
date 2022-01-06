90af21a29156d06ccb896473e9ac26b5178f4c71
Vasilii Rogin <vasya@rogin.ru>
2022-01-06 15:06:35 +0200

    Params settings

--- src/ui/editor/core/overlays/questSettings/paramsSettings.tsx
+++ src/ui/editor/core/overlays/questSettings/paramsSettings.tsx
@@ -1,16 +1,74 @@
 import classNames from "classnames";
 import * as React from "react";
+import { DeepImmutable } from "../../../../../lib/qmplayer/deepImmutable";
 import { Quest } from "../../../../../lib/qmplayer/funcs";
+import { QMParam } from "../../../../../lib/qmreader";
 import { addParameter, removeLastParameter } from "../../actions";
 import { getParamStringInfo } from "../../hovers/paramsAndChangeConditionsSummary";
 import { QuestSettingsTabProps } from "./props";
+
+function QuestParamSettings({
+  param,
+  setParam,
+}: {
+  param: DeepImmutable<QMParam>;
+  setParam: (newParam: DeepImmutable<QMParam>) => void;
+}) {
+  return (
+    <div>
+      <div className="d-flex align-items-center justify-content-between mb-2">
+        <label className="form-check-label ml-4">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            checked={param.active}
+            onChange={(e) =>
+              setParam({
+                ...param,
+                active: !param.active,
+              })
+            }
+          />
+          Включен
+        </label>
+
+        <label className="form-check-label ml-4">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            checked={param.showWhenZero}
+            onChange={(e) =>
+              setParam({
+                ...param,
+                showWhenZero: !param.showWhenZero,
+              })
+            }
+            disabled={!param.active}
+          />
+          Показывать при нуле
+        </label>
+      </div>
+      <div className="d-flex align-items-center mb-2">
+        <label className="form-check-label" style={{ flexShrink: 0 }}>
+          Рабочее название
+        </label>
+        <input
+          disabled={!param.active}
+          className="form-control w-100 ml-2"
+          value={param.name}
+          onChange={(e) => setParam({ ...param, name: e.target.value })}
+        />
+      </div>
+    </div>
+  );
+}
 
 export function QuestParamsSettings({ quest, setQuest }: QuestSettingsTabProps) {
   const [paramId, setParamId] = React.useState(0);
 
   return (
     <>
-      <div className="row mt-1">
+      <div className="row mt-3">
         <div className="col-6">
           <select
             className="form-control"
@@ -59,6 +117,18 @@ export function QuestParamsSettings({ quest, setQuest }: QuestSettingsTabProps) 
               Добавить параметр
             </button>
           </div>
+        </div>
+
+        <div className="col-6">
+          <QuestParamSettings
+            param={quest.params[paramId]}
+            setParam={(newParam) => {
+              setQuest({
+                ...quest,
+                params: quest.params.map((param, idx) => (idx === paramId ? newParam : param)),
+              });
+            }}
+          />
         </div>
       </div>
     </>
