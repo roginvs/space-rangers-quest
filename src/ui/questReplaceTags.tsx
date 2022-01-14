21002e34cae93c49c8f1f2c85e45f5cbe97ce02a
Vasilii Rogin <vasya@rogin.ru>
2022-01-14 16:32:09 +0200

    Add color support

--- src/ui/questReplaceTags.tsx
+++ src/ui/questReplaceTags.tsx
@@ -3,6 +3,14 @@ import * as React from "react";
 import { formatTokens } from "./questReplaceTags.format";
 import { splitStringToTokens } from "./questReplaceTags.split";
 
+/**
+ * Renders text, understand tags:
+ *   - <clr> highlight me <clrEnd>
+ *   - <clr> highlight me too </clr>
+ *   - <fix> use monospace font </fix>
+ *   - <format=left,30>  add spaced in the left (or right or center) </format>
+ *   - <color=R,G,B> Use color </color>
+ */
 export function QuestReplaceTags(props: { str: string }) {
   const tags = formatTokens(splitStringToTokens(props.str));
 
@@ -18,7 +26,12 @@ export function QuestReplaceTags(props: { str: string }) {
             "game-fix": tag.isFix,
             "text-success": tag.type === "text" ? tag.isClr : undefined,
           })}
-          // TODO color
+          style={{
+            color:
+              tag.type === "text" && tag.color
+                ? `rgb(${tag.color.r}, ${tag.color.g}, ${tag.color.b})`
+                : undefined,
+          }}
         >
           {tag.type === "text" ? (
             tag.text
