2d531860941a901585096916d3be68efdabe6326
Vasilii Rogin <vasya@rogin.ru>
2022-01-20 17:21:13 +0200

    Adding new frame

--- src/ui/questPlay/questPlay.frame.tsx
+++ src/ui/questPlay/questPlay.frame.tsx
@@ -67,19 +67,19 @@ export const QuestPlayFrame: React.FC<{
 
 /*
 
-  CORNER_HEIGHT
+  CORNER_HEIGHT_2
 /
 |
 |
 |            <----> CORNER_WIDTH_2           
 |
 |       <----> CORNER_WIDTH
-|_      ______________________v 
-^       |    |    |           |
-|       |    |____|___________|
+|       ______________________v_
+|       |    |    |           |
+v_      |____|____|___________|_
 |       |    |                ^
-v_      |____|                \ CORNER_HEIGHT_2
-        |    |
+|_      |____|                \ CORNER_HEIGHT
+^       |    |
         |    |
         |
         |
@@ -87,20 +87,73 @@ v_      |____|                \ CORNER_HEIGHT_2
         |
 */
 
-const CORNER_WIDTH = 80;
-const CORNER_HEIGHT = 180;
-const CORNER_WIDTH_2 = 100;
-const CORNER_HEIGHT_2 = 55;
+const CORNER_WIDTH = 75;
+const CORNER_HEIGHT = 55;
+const CORNER_WIDTH_2 = 80;
+const CORNER_HEIGHT_2 = 120;
+
+const IMAGE_PADDING_X = 20;
+const IMAGE_PADDING_Y = 25;
 
 export const QuestPlayFrame2: React.FC<{
   height: number | string;
 }> = ({ children, height }) => {
+  const DEBUG_BORDER = "1px solid black";
   return (
     <div
       style={{
         height,
         width: "100%",
+        position: "relative",
       }}
-    ></div>
+    >
+      {children}
+      <div
+        style={{
+          position: "absolute",
+          left: 0,
+          top: 0,
+          width: CORNER_WIDTH,
+          height: CORNER_HEIGHT + CORNER_HEIGHT_2,
+          border: DEBUG_BORDER,
+          background: "url('/questplay/frame.png')",
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          left: CORNER_WIDTH,
+          top: 0,
+          width: CORNER_WIDTH_2,
+          height: CORNER_HEIGHT,
+          border: DEBUG_BORDER,
+          background: `url('/questplay/frame.png') -${CORNER_WIDTH}px 0px`,
+        }}
+      />
+
+      <div
+        style={{
+          position: "absolute",
+          left: 0,
+          bottom: 0,
+          width: CORNER_WIDTH,
+          height: CORNER_HEIGHT + CORNER_HEIGHT_2,
+          border: DEBUG_BORDER,
+          background: `url('/questplay/frame.png') left bottom`,
+        }}
+      />
+
+      <div
+        style={{
+          position: "absolute",
+          left: CORNER_WIDTH,
+          bottom: 0,
+          width: CORNER_WIDTH_2,
+          height: CORNER_HEIGHT,
+          border: DEBUG_BORDER,
+          background: `url('/questplay/frame.png') -${CORNER_WIDTH}px bottom`,
+        }}
+      />
+    </div>
   );
 };
