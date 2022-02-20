81d251bf391b4d0151daa58eed4df4b31d8c6edc
Vasilii Rogin <vasya@rogin.ru>
2022-02-20 22:21:51 +0200

    Fix ios fullscreen

--- src/ui/questPlay/fullscreen.ts
+++ src/ui/questPlay/fullscreen.ts
@@ -5,7 +5,19 @@
  */
 export function toggleFullscreen(forceFullscreenState?: boolean) {
   if (!document.fullscreenElement && forceFullscreenState !== false) {
-    document.documentElement.requestFullscreen().catch((e) => console.warn(e));
+    const elem = document.documentElement;
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen().catch((e) => console.warn(e));
+    } else {
+      const browserPrefixes = ["webkit", "moz", "ms", "o"];
+      const unknownElem = elem as unknown as Record<string, unknown>;
+      for (const key of browserPrefixes.map((prefix) => `${prefix}RequestFullScree`)) {
+        const func = unknownElem[key];
+        if (typeof func === "function") {
+          func();
+        }
+      }
+    }
   } else {
     if (document.exitFullscreen && forceFullscreenState !== true) {
       document.exitFullscreen().catch((e) => console.warn(e));
