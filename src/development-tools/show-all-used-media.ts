86482c600f7a09c84029400e0ae4a42070606cfa
Vasilii Rogin <vasya@rogin.ru>
2022-02-26 19:53:45 +0200

    Fix prettier

--- src/development-tools/show-all-used-media.ts
+++ src/development-tools/show-all-used-media.ts
@@ -4,7 +4,6 @@ import * as fs from "fs";
 import { QMPlayer } from "../lib/qmplayer";
 import { getAllMediaFromQmm } from "../lib/getAllMediaFromQmm";
 
-
 /*
 
 for f in *.png; do convert $f -quality 90 $(basename $f .png).jpg; done
@@ -15,17 +14,16 @@ const data = fs.readFileSync(process.argv[2]);
 
 const quest = parse(data);
 
-
 const media = getAllMediaFromQmm(quest);
 
 console.info(`All images:`);
-Object.keys(media.images).forEach(img => console.info(img));
-console.info('');
+Object.keys(media.images).forEach((img) => console.info(img));
+console.info("");
 
 console.info(`All sounds:`);
-Object.keys(media.sounds).forEach(sound => console.info(sound));
-console.info('');
+Object.keys(media.sounds).forEach((sound) => console.info(sound));
+console.info("");
 
 console.info(`All track:`);
-Object.keys(media.tracks).forEach(track => console.info(track));
-console.info('');
+Object.keys(media.tracks).forEach((track) => console.info(track));
+console.info("");
