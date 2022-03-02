5d26afa59d547520356648f1cdeec3dfba689983
Vasilii Rogin <vasya@rogin.ru>
2022-03-02 23:03:37 +0200

    Small refactor of options

--- src/ui/options.tsx
+++ src/ui/options.tsx
@@ -5,14 +5,10 @@ import { DB } from "./db/db";
 import { Player, Lang } from "../lib/qmplayer/player";
 import { observer } from "mobx-react";
 import { Store } from "./store";
+import { toJS } from "mobx";
 
 interface OptionsState {
-  Ranger: string;
-  FromPlanet: string;
-  FromStar: string;
-  ToPlanet: string;
-  ToStar: string;
-  lang: Lang;
+  player: Player;
 
   busy: boolean;
 }
@@ -25,7 +21,7 @@ export class OptionsTabContainer extends React.Component<
   OptionsState
 > {
   state = {
-    ...this.props.store.player,
+    player: toJS(this.props.store.player),
     busy: false,
   };
   render() {
@@ -33,6 +29,16 @@ export class OptionsTabContainer extends React.Component<
     if (this.state.busy) {
       return <Loader text={l.saving} />;
     }
+
+    const player = this.state.player;
+    const updatePlayer = <K extends keyof Player>(key: K, value: Player[K]) => {
+      this.setState({
+        player: {
+          ...player,
+          [key]: value,
+        },
+      });
+    };
     return (
       <DivFadeinCss key="options" className="text-center my-3 container">
         <form>
@@ -43,8 +49,8 @@ export class OptionsTabContainer extends React.Component<
                 type="text"
                 className="form-control"
                 placeholder=""
-                value={this.state.Ranger}
-                onChange={(e) => this.setState({ Ranger: e.target.value })}
+                value={player.Ranger}
+                onChange={(e) => updatePlayer("Ranger", e.target.value)}
               />
             </div>
 
@@ -52,8 +58,8 @@ export class OptionsTabContainer extends React.Component<
               <label>{l.lang}</label>
               <select
                 className="form-control"
-                value={this.state.lang}
-                onChange={(e) => this.setState({ lang: e.target.value as Lang })}
+                value={player.lang}
+                onChange={(e) => updatePlayer("lang", e.target.value as Lang)}
               >
                 <option value="rus">{l.rus}</option>
                 <option value="eng">{l.eng}</option>
@@ -66,8 +72,8 @@ export class OptionsTabContainer extends React.Component<
                 type="text"
                 className="form-control"
                 placeholder=""
-                value={this.state.FromPlanet}
-                onChange={(e) => this.setState({ FromPlanet: e.target.value })}
+                value={player.FromPlanet}
+                onChange={(e) => updatePlayer("FromPlanet", e.target.value)}
               />
             </div>
 
@@ -77,8 +83,8 @@ export class OptionsTabContainer extends React.Component<
                 type="text"
                 className="form-control"
                 placeholder=""
-                value={this.state.FromStar}
-                onChange={(e) => this.setState({ FromStar: e.target.value })}
+                value={player.FromStar}
+                onChange={(e) => updatePlayer("FromStar", e.target.value)}
               />
             </div>
 
@@ -88,8 +94,8 @@ export class OptionsTabContainer extends React.Component<
                 type="text"
                 className="form-control"
                 placeholder=""
-                value={this.state.ToPlanet}
-                onChange={(e) => this.setState({ ToPlanet: e.target.value })}
+                value={player.ToPlanet}
+                onChange={(e) => updatePlayer("ToPlanet", e.target.value)}
               />
             </div>
 
@@ -99,8 +105,8 @@ export class OptionsTabContainer extends React.Component<
                 type="text"
                 className="form-control"
                 placeholder=""
-                value={this.state.ToStar}
-                onChange={(e) => this.setState({ ToStar: e.target.value })}
+                value={player.ToStar}
+                onChange={(e) => updatePlayer("ToStar", e.target.value)}
               />
             </div>
           </div>
@@ -114,22 +120,13 @@ export class OptionsTabContainer extends React.Component<
               });
               (async () => {
                 const db = this.props.store.db;
-                await db.setConfigBoth("player", {
-                  Ranger: this.state.Ranger,
-                  Money: this.props.store.player.Money,
-                  Player: this.state.Ranger,
-                  FromPlanet: this.state.FromPlanet,
-                  FromStar: this.state.FromStar,
-                  ToPlanet: this.state.ToPlanet,
-                  ToStar: this.state.ToStar,
-                  lang: this.state.lang,
-                });
-                await db.setOwnHighscoresName(this.state.Ranger);
-                const player = await db.getConfigLocal("player");
-                if (!player) {
+                await db.setConfigBoth("player", player);
+                await db.setOwnHighscoresName(player.Ranger);
+                const savedPlayer = await db.getConfigLocal("player");
+                if (!savedPlayer) {
                   throw new Error("Where is the player?");
                 }
-                this.props.store.player = player;
+                this.props.store.player = savedPlayer;
                 this.setState({
                   busy: false,
                 });
